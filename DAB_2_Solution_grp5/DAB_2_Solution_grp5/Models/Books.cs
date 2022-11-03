97c7ec18b21bf6bb38cb0fba4fd17350ebff9b13
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-11-03 10:22:52 +0100

    Facility

--- DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Books.cs
+++ DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Books.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAB_2_Solution_grp5.Models
 {
-    internal class Books
+    public class Books
     {
+
+        
+       
+
+
+
+
+
+
+
+
     }
+
 }
