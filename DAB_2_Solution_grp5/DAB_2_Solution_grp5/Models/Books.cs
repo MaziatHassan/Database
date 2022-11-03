fde68f86abcd0a0eebf34799f58a0fcaf443c5fd
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-11-03 10:38:44 +0100

    Revert "Merge branch 'main' of https://github.com/MaziatHassan/Database"
    This reverts commit c71f638e69374f2cae4fd10d37fcdf2114ef3bcf, reversing
    changes made to ccf039a7d73d5932c42c5850ec25eaeea088ecf1.

--- DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Books.cs
+++ DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Books.cs
@@ -1,25 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAB_2_Solution_grp5.Models
 {
-    public class Books
+    internal class Books
     {
-
-        
-       
-
-
-
-
-
-
-
-
     }
-
 }
--- DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Facility.cs
+++ DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/Facility.cs
@@ -1,27 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAB_2_Solution_grp5.Models
 {
-    public class Facility
+    internal class Facility
     {
-
-
-        [Key]
-        public int id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-        public string? Type { get; set; }
-        public string? Description { get; set; }
-        public string? Bookable { get; set; }
-        public string? Item { get; set; }
-
-
-
-
     }
 }
