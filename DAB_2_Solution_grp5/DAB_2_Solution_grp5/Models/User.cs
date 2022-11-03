263631f59c501e7b6ea82d8e967714c954e4e6de
RasWest <83766648+RasWest@users.noreply.github.com>
2022-11-03 10:17:04 +0100

    Added atributes for User

--- DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/User.cs
+++ DAB_2_Solution_grp5/DAB_2_Solution_grp5/Models/User.cs
@@ -8,5 +8,12 @@ namespace DAB_2_Solution_grp5.Models
 {
     internal class User
     {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Category  { get; set; }
+        public string CVR { get; set; }
+
     }
 }
